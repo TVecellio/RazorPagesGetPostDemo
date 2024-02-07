9eae15c9c4ff04b4123e7b00edb1ced28bf550a8
Thomas Vecellio <tjv00008@mix.wvu.edu>
2024-02-07 12:01:01 -0500

    fixed other issues

--- RazorPagesGetPostDemoUI/Pages/Person/AddInfo.cshtml.cs
+++ RazorPagesGetPostDemoUI/Pages/Person/AddInfo.cshtml.cs
@@ -8,14 +8,18 @@ namespace RazorPagesGetPostDemoUI.Pages.Person
     public class AddInfoModel : PageModel
     {
         private readonly ILogger<AddInfoModel> _logger;
-
+        public AddInfoModel(ILogger<AddInfoModel> logger)
+        {
+            _logger = logger;
+        }
         [BindProperty(SupportsGet = true)]
         public PersonInfo CurrentPerson { get; set; } = new(); // can use new if the type is mentioned on the left
         public void OnGet()
-        {
+        { 
+        
             _logger.LogInformation("ONGET called in ADDINFO page");
         }
-        public IActionResult onPost()
+        public IActionResult OnPost()
         {
             _logger.LogInformation("ONPOST called in ADDINFO page");
             if(ModelState.IsValid)
